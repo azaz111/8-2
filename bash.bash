e4eccb7c93a69cfc2f73a317a13e5a0c6d2acd58
azaz111 <86608104+azaz111@users.noreply.github.com>
2021-11-10 12:35:28 +0300

    Update bash.bash

--- bash.bash
+++ bash.bash
@@ -43,14 +43,14 @@ cd
 parted /dev/nvme1n1 --script mklabel gpt mkpart xfspart xfs 0% 100%
 mkfs.xfs -f /dev/nvme1n1
 partprobe /dev/nvme1n1
-mkdir /disk1
-mount /dev/nvme1n1 /disk1
+mkdir /disk4
+mount /dev/nvme1n1 /disk4
 # Создаем дериктории на дисках
-cd /disk1
+cd /disk4
 mkdir video
-cd /disk1
+cd /disk4
 mkdir vid1
-cd /disk1
+cd /disk4
 mkdir vid2
 cd /root
 # Качаем плоттер и устанавливаем 
@@ -99,7 +99,7 @@ screen -dmS otpravka_na_serv ./otpravka.sh
 # ЗАпуск Плотера ------------------------------
 screen -dmS Copi1 ./Copi1.sh
 screen -dmS Copi ./Copi.sh
-screen -dmS videorender1 ./chia-plotter/build/chia_plot -n -1 -r 8 -u 256 -t /disk1/vid1/ -2 /disk1/vid2/ -d /disk1/video/ -f b8e1d57e3e2dbb40ac8f2b257b762d05fcfc5b79c32a22255424644b7d183daa7c454624783f2d959c02eb1d2a4ba3a3 -p 91ea997633345082b15f83b957449180037030b6b7485f07ed4ee7558d08d3efbccf2c3d68ba724f5b3a8281a0055e27
+screen -dmS videorender1 ./chia-plotter/build/chia_plot -n -1 -r 8 -u 256 -t /disk4/vid1/ -2 /disk4/vid2/ -d /disk4/video/ -f b8e1d57e3e2dbb40ac8f2b257b762d05fcfc5b79c32a22255424644b7d183daa7c454624783f2d959c02eb1d2a4ba3a3 -p 91ea997633345082b15f83b957449180037030b6b7485f07ed4ee7558d08d3efbccf2c3d68ba724f5b3a8281a0055e27
 screen -dmS trans ./trans.sh
 screen -dmS otchet python3 awsstat.py
 screen -r trans
